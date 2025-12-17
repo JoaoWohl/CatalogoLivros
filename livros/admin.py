45f8ecbe95658636843c81bcf68a189beb8b28ec
JoaoWohl <joaovitorsantoswohl9@gmail.com>
2025-12-17 18:25:31 -0300

    criando app dos livors e models

--- livros/admin.py
+++ livros/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Categoria, Autor, Livro, Editora
+
 
 # Register your models here.
+
+admin.site.register([Categoria, Autor, Livro, Editora])
--- livros/models.py
+++ livros/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.nome
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=160)
+
+    class Meta:
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        return self.nome
+    
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return self.nome
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=128)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE, default="")
+    lancamento = models.DateField(default=date.today)
+
+    def __str__(self):
+        return self.titulo
